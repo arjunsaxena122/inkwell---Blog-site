de5dd3183c3a0d684cb1b08ff8b64e8deff87298
arjunsaxena122 <arjunsaxena122@gmail.com>
2025-07-16 11:45:58 +0530

    Revert "ADD: Initial Commit"
    This reverts commit f5ac6cf1aea7739b517b04c9dc03d0271e887e3a.

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-import express, { Application } from "express";
-import { customErrorHandler } from "./middlewares/error.middleware";
-
-const app: Application = express();
-
-app.use(customErrorHandler);
-
-export default app;
